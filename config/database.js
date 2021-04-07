36bd4be8ccf3859e1a8b80be435c270f987c4095
Jin Zhao <sunrisemoment@outlook.com>
2021-04-07 21:06:04 +0800

    remove db credentials

--- config/database.js
+++ config/database.js
@@ -1,19 +1,3 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
     defaultConnection: 'default',
     connections: {
@@ -24,9 +8,9 @@ module.exports = ({ env }) => ({
                 host: env('DATABASE_HOST', '127.0.0.1'),
                 // host: env('DATABASE_HOST', 'ec2-54-241-141-30.us-west-1.compute.amazonaws.com'),
                 port: env.int('DATABASE_PORT', 5432),
-                database: env('DATABASE_NAME', 'sunsfeed'),
-                username: env('DATABASE_USERNAME', 'postgres'),
-                password: env('DATABASE_PASSWORD', 'Admin@123'),
+                database: env('DATABASE_NAME'),
+                username: env('DATABASE_USERNAME'),
+                password: env('DATABASE_PASSWORD'),
                 ssl: env.bool('DATABASE_SSL', false),
             },
             options: {}
